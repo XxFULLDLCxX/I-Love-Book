11c734f5ed2c86b89f511c7c85ecb77340e439c8
José Matheus <XxFULLDLCxX@outlook.com>
2023-09-22 16:55:23 -0300

    feat: Implemented Prisma, replacing pg for database management

---
+++ src/repositories/books-repository.ts
@@ -0,0 +1,27 @@
+import { CreateBook } from '../protocols/book';
+import { CreateReview } from '../protocols/review';
+
+import prisma from '../database';
+
+export async function getBooks() {
+  const result = await prisma.books.findMany();
+  return result;
+}
+
+export async function getBook(id: number) {
+  const result = await prisma.books.findMany({
+    where: { id },
+  });
+  return result;
+}
+
+export async function createBook(book: CreateBook) {
+  const result = await prisma.books.create({ data: book });
+  return result;
+}
+
+export async function reviewBook(bookReview: CreateReview) {
+  const { bookId, grade, review } = bookReview;
+  const result = await prisma.books.update({ data: { grade, review, read: true }, where: { id: bookId } });
+  return result;
+}
---
+++ src/schemas/book-schema.ts
@@ -0,0 +1,10 @@
+import joi, { valid } from "joi";
+import { CreateBook } from "../protocols/book";
+
+export const bookSchema = joi.object<CreateBook>({
+  title: joi.string().required(),
+  author: joi.string().required(),
+  cover: joi.string().uri(),
+  publisher: joi.string().required(),
+  purchaseDate: joi.date().required()
+});
